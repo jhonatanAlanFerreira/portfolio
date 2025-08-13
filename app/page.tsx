bbef8c4a511c43c2d16bf38390c147154f5eea40
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-08-13 19:45:15 -0300

    create aside WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <aside className="flex-1 backdrop-blur-md shadow-lg bg-black/45 shadow-black/80 border border-white/10 opacity-70"></aside>
-      <main className="flex-[3]">
+      <aside className="flex flex-1 pt-15 bg-black/30 backdrop-blur-md border-r border-white/10 shadow-lg">
+        <div className="w-full h-fit flex flex-col gap-5 items-center">
+          <div className="overflow-hidden rounded-full h-32 w-32 border-2 border-white shadow-md">
+            <img src="profile.png" alt="Profile Picture" />
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <h1 className="text-white text-3xl font-bold">Jhonatan Ferreira</h1>
+            <p className="text-gray-500">Full Stack Developer</p>
+          </div>
+        </div>
+      </aside>
+      <main className="flex-3">
         <section></section>
       </main>
     </div>
