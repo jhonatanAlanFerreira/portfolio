d3aa8f48fbac109406886badbcac93be4af6c6cf
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-08-13 00:07:59 -0300

    create initial structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>HI</h1>;
+  return (
+    <div className="flex h-screen">
+      <aside className="flex-1 backdrop-blur-md shadow-lg bg-black/45 shadow-black/80 border border-white/10 opacity-70"></aside>
+      <main className="flex-[3]">
+        <section></section>
+      </main>
+    </div>
+  );
 }
