805f396b8ab3ed851b0f88b41bb3e4be4a9a4e23
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-08-16 13:56:56 -0300

    create aside WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import TechCarousel from "@/components/TechCarousel/TechCarousel";
+
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <aside className="flex flex-1 pt-15 bg-black/30 backdrop-blur-md border-r border-white/10 shadow-lg">
+      <aside className="flex flex-1 pt-10 bg-black/30 backdrop-blur-md border-r border-white/5 shadow-lg overflow-hidden">
         <div className="w-full h-fit flex flex-col gap-5 items-center">
           <div className="overflow-hidden rounded-full h-32 w-32 border-2 border-white shadow-md">
             <img src="profile.png" alt="Profile Picture" />
@@ -10,9 +12,12 @@ export default function Home() {
             <h1 className="text-white text-3xl font-bold">Jhonatan Ferreira</h1>
             <p className="text-gray-500">Full Stack Developer</p>
           </div>
+          <div className="w-full px-2">
+            <TechCarousel></TechCarousel>
+          </div>
         </div>
       </aside>
-      <main className="flex-3">
+      <main className="flex flex-3">
         <section></section>
       </main>
     </div>
---
+++ components/TechCarousel/TechCarousel.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  FaReact,
+  FaNodeJs,
+  FaAngular,
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+} from "react-icons/fa";
+
+export default function TechCarousel() {
+  const icons = [
+    <FaReact className="text-sky-400" />,
+    <FaNodeJs className="text-green-500" />,
+    <FaAngular className="text-red-500" />,
+    <FaHtml5 className="text-orange-500" />,
+    <FaCss3Alt className="text-blue-500" />,
+    <FaJsSquare className="text-yellow-400" />,
+  ];
+
+  return (
+    <div className="marquee rounded-xl shadow-sm bg-gradient-to-b from-transparent to-slate-700/70 hover:to-slate-600/70 border-[0.1px] border-slate-500/60 hover:border-slate-300/60 transition-colors duration-300">
+      <p className="text-white absolute pl-4 pt-2">Tech Stack</p>
+      <div className="marquee__item text-white pt-8">
+        <div className="flex">
+          {icons.map((icon, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center w-20 h-20 grayscale"
+            >
+              {icon && React.cloneElement(icon, { size: 40 })}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="marquee__item text-white pt-8">
+        <div className="flex">
+          {icons.map((icon, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-center w-20 h-20 grayscale"
+            >
+              {icon && React.cloneElement(icon, { size: 40 })}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
