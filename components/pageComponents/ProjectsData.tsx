7972f650ec1647fa7d219e1607c1bbd754e3b52c
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-08-29 19:55:43 -0300

    separate icons into a dedicated file

--- components/pageComponents/ProjectsData.tsx
+++ components/pageComponents/ProjectsData.tsx
@@ -1,27 +1,10 @@
-import { BsRegex } from "react-icons/bs";
 import {
-  FaReact,
-  FaNodeJs,
-  FaAngular,
-  FaHtml5,
-  FaCss3Alt,
-  FaJsSquare,
-  FaDocker,
-  FaGitAlt,
-} from "react-icons/fa";
-import { RiRemixRunFill } from "react-icons/ri";
-import {
-  SiTypescript,
-  SiTailwindcss,
-  SiMongodb,
-  SiGraphql,
-  SiMysql,
-  SiBootstrap,
-  SiPrisma,
-  SiExpress,
-  SiSqlite,
-  SiReact,
-} from "react-icons/si";
+  developer_manager_icons,
+  financial_manager_icons,
+  git_search_icons,
+  microservices_dotnet_icons,
+  react_native_simple_todo_icons,
+} from "./ProjectsIcons";
 
 export const ProjectsData = [
   {
@@ -36,68 +19,7 @@ export const ProjectsData = [
     description: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
                 faucibus ex sapien vitae pellentesque sem placerat. In id cursus
                 mi pretium tellus duis convallis.`,
-    stackIcons: [
-      {
-        icon: <FaReact className="text-sky-400" />,
-        name: "React",
-        link: "https://react.dev/",
-      },
-      {
-        icon: <FaNodeJs className="text-green-500" />,
-        name: "Node.js",
-        link: "https://nodejs.org/en/docs",
-      },
-      {
-        icon: <FaHtml5 className="text-orange-400" />,
-        name: "HTML5",
-        link: "https://developer.mozilla.org/docs/Web/HTML",
-      },
-      {
-        icon: <FaCss3Alt className="text-blue-400" />,
-        name: "CSS3",
-        link: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        icon: <SiTypescript className="text-blue-600" />,
-        name: "TypeScript",
-        link: "https://www.typescriptlang.org/docs/",
-      },
-      {
-        icon: <SiTailwindcss className="text-sky-500" />,
-        name: "Tailwind CSS",
-        link: "https://tailwindcss.com/docs",
-      },
-      {
-        icon: <SiMongodb className="text-green-600" />,
-        name: "MongoDB",
-        link: "https://www.mongodb.com/docs/",
-      },
-      {
-        icon: <SiGraphql className="text-pink-500" />,
-        name: "GraphQL",
-        link: "https://graphql.org/learn/",
-      },
-      {
-        icon: <RiRemixRunFill className="text-purple-500" />,
-        name: "Remix",
-        link: "https://remix.run/docs",
-      },
-      {
-        icon: <FaDocker className="text-blue-400" />,
-        name: "Docker",
-        link: "https://docs.docker.com/",
-      },
-      {
-        icon: <FaGitAlt className="text-orange-500" />,
-        name: "Git",
-        link: "https://git-scm.com/doc",
-      },
-      {
-        icon: <SiPrisma className="text-cyan-600" />,
-        name: "Prisma",
-        link: "https://www.prisma.io/docs",
-      },
-    ],
+    stackIcons: financial_manager_icons,
   },
   {
     name: "React Native Simple Todo (2025)",
@@ -111,43 +33,7 @@ export const ProjectsData = [
     description: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
                 faucibus ex sapien vitae pellentesque sem placerat. In id cursus
                 mi pretium tellus duis convallis.`,
-    stackIcons: [
-      {
-        icon: <SiReact className="text-cyan-400" />,
-        name: "React Native",
-        link: "https://reactnative.dev/docs/getting-started",
-      },
-      {
-        icon: <FaCss3Alt className="text-blue-400" />,
-        name: "CSS3",
-        link: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        icon: <FaJsSquare className="text-yellow-400" />,
-        name: "JavaScript",
-        link: "https://developer.mozilla.org/docs/Web/JavaScript",
-      },
-      {
-        icon: <SiTypescript className="text-blue-600" />,
-        name: "TypeScript",
-        link: "https://www.typescriptlang.org/docs/",
-      },
-      {
-        icon: <SiTailwindcss className="text-sky-500" />,
-        name: "NativeWind",
-        link: "https://www.nativewind.dev/",
-      },
-      {
-        icon: <SiSqlite className="text-blue-500" />,
-        name: "SQLite",
-        link: "https://www.sqlite.org/docs.html",
-      },
-      {
-        icon: <FaGitAlt className="text-orange-500" />,
-        name: "Git",
-        link: "https://git-scm.com/doc",
-      },
-    ],
+    stackIcons: react_native_simple_todo_icons,
   },
   {
     name: "Microservices Dotnet (2023)",
@@ -160,43 +46,7 @@ export const ProjectsData = [
     description: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
                 faucibus ex sapien vitae pellentesque sem placerat. In id cursus
                 mi pretium tellus duis convallis.`,
-    stackIcons: [
-      {
-        icon: <FaHtml5 className="text-orange-400" />,
-        name: "HTML5",
-        link: "https://developer.mozilla.org/docs/Web/HTML",
-      },
-      {
-        icon: <FaCss3Alt className="text-blue-400" />,
-        name: "CSS3",
-        link: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        icon: <FaJsSquare className="text-yellow-400" />,
-        name: "JavaScript",
-        link: "https://developer.mozilla.org/docs/Web/JavaScript",
-      },
-      {
-        icon: <FaDocker className="text-blue-400" />,
-        name: "Docker",
-        link: "https://docs.docker.com/",
-      },
-      {
-        icon: <FaGitAlt className="text-orange-500" />,
-        name: "Git",
-        link: "https://git-scm.com/doc",
-      },
-      {
-        icon: <SiMysql className="text-blue-500" />,
-        name: "MySQL",
-        link: "https://dev.mysql.com/doc/",
-      },
-      {
-        icon: <SiBootstrap className="text-purple-600" />,
-        name: "Bootstrap",
-        link: "https://getbootstrap.com/docs/",
-      },
-    ],
+    stackIcons: microservices_dotnet_icons,
   },
   {
     name: "Git Search (2022)",
@@ -209,48 +59,7 @@ export const ProjectsData = [
     description: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
                 faucibus ex sapien vitae pellentesque sem placerat. In id cursus
                 mi pretium tellus duis convallis.`,
-    stackIcons: [
-      {
-        icon: <FaReact className="text-sky-400" />,
-        name: "React",
-        link: "https://react.dev/",
-      },
-      {
-        icon: <FaNodeJs className="text-green-500" />,
-        name: "Node.js",
-        link: "https://nodejs.org/en/docs",
-      },
-      {
-        icon: <FaHtml5 className="text-orange-400" />,
-        name: "HTML5",
-        link: "https://developer.mozilla.org/docs/Web/HTML",
-      },
-      {
-        icon: <FaCss3Alt className="text-blue-400" />,
-        name: "CSS3",
-        link: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        icon: <FaJsSquare className="text-yellow-400" />,
-        name: "JavaScript",
-        link: "https://developer.mozilla.org/docs/Web/JavaScript",
-      },
-      {
-        icon: <FaGitAlt className="text-orange-500" />,
-        name: "Git",
-        link: "https://git-scm.com/doc",
-      },
-      {
-        icon: <BsRegex className="text-teal-400" />,
-        name: "Regex",
-        link: "https://developer.mozilla.org/docs/Web/JavaScript/Guide/Regular_expressions",
-      },
-      {
-        icon: <SiBootstrap className="text-purple-600" />,
-        name: "Bootstrap",
-        link: "https://getbootstrap.com/docs/",
-      },
-    ],
+    stackIcons: git_search_icons,
   },
   {
     name: "Developer Manager (2022)",
@@ -263,57 +72,6 @@ export const ProjectsData = [
     description: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
                 faucibus ex sapien vitae pellentesque sem placerat. In id cursus
                 mi pretium tellus duis convallis.`,
-    stackIcons: [
-      {
-        icon: <FaNodeJs className="text-green-500" />,
-        name: "Node.js",
-        link: "https://nodejs.org/en/docs",
-      },
-      {
-        icon: <FaAngular className="text-red-500" />,
-        name: "Angular",
-        link: "https://angular.io/docs",
-      },
-      {
-        icon: <FaHtml5 className="text-orange-400" />,
-        name: "HTML5",
-        link: "https://developer.mozilla.org/docs/Web/HTML",
-      },
-      {
-        icon: <FaCss3Alt className="text-blue-400" />,
-        name: "CSS3",
-        link: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        icon: <SiTypescript className="text-blue-600" />,
-        name: "TypeScript",
-        link: "https://www.typescriptlang.org/docs/",
-      },
-      {
-        icon: <SiMongodb className="text-green-600" />,
-        name: "MongoDB",
-        link: "https://www.mongodb.com/docs/",
-      },
-      {
-        icon: <FaDocker className="text-blue-400" />,
-        name: "Docker",
-        link: "https://docs.docker.com/",
-      },
-      {
-        icon: <FaGitAlt className="text-orange-500" />,
-        name: "Git",
-        link: "https://git-scm.com/doc",
-      },
-      {
-        icon: <SiBootstrap className="text-purple-600" />,
-        name: "Bootstrap",
-        link: "https://getbootstrap.com/docs/",
-      },
-      {
-        icon: <SiExpress className="text-gray-800" />,
-        name: "Express",
-        link: "https://expressjs.com/",
-      },
-    ],
+    stackIcons: developer_manager_icons,
   },
 ];
