4f8dd8237db7d24122d71944bb1db1fea9016c3f
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-09-15 15:02:02 -0300

    create function to send email

--- components/pageComponents/Contact/ContactActions.ts
+++ components/pageComponents/Contact/ContactActions.ts
@@ -1,12 +1,29 @@
 "use server";
 import { ServerResponse } from "@/types/ServerResponse";
 import { ContactData } from "./ContactInterfaces";
+import { Resend } from "resend";
 
 export async function sendEmailAction(
   data: ContactData
 ): Promise<ServerResponse> {
+  const { name, email, message } = data;
+  const { RESEND_FROM_EMAIL, RESEND_TO_EMAIL, RESEND_API_KEY } = process.env;
+
+  if (!RESEND_FROM_EMAIL || !RESEND_TO_EMAIL || !RESEND_API_KEY) {
+    console.error("Env variable missing");
+    return { success: false, message: "Failed to send email." };
+  }
+
+  const resend = new Resend(RESEND_API_KEY);
+
   try {
-    console.log("Received data for email:", data);
+    await resend.emails.send({
+      from: RESEND_FROM_EMAIL,
+      to: RESEND_TO_EMAIL,
+      subject: `Message from ${name}`,
+      text: `${message}\n\nReply to: ${email}`,
+    });
+
     return { success: true, message: "Email sent successfully!" };
   } catch (error) {
     return { success: false, message: "Failed to send email." };
