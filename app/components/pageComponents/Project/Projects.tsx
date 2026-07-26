2004775186c33cf5d8e925589c21a89c4a5b56e1
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2026-07-25 22:51:36 -0300

    Improve the card layout

--- app/components/pageComponents/Project/Projects.tsx
+++ app/components/pageComponents/Project/Projects.tsx
@@ -52,88 +52,89 @@ export default function Projects() {
                   ></iframe>
                 </div>
               )}
-              <div className="flex flex-3 flex-col space-y-2 text-[var(--muted)] lg:space-y-5">
-                <div>
-                  <h3 className="text-md font-medium text-[var(--primary)] lg:text-xl">
-                    {pd.name}
-                  </h3>
-                  <p className="text-[10px] text-[var(--primary)]/80 lg:text-sm">
-                    {pd.description}
-                  </p>
-                </div>
-
-                <div className="text-sm font-medium text-[var(--muted)] lg:text-xl">
-                  <div className="flex flex-col">
-                    <span>Language</span>
-                    <span className="text-[var(--primary)]/80">
-                      {pd.language}
-                    </span>
+              <div className="flex flex-3 flex-col justify-between text-[var(--muted)]">
+                <div className="mb-2 flex flex-col gap-2">
+                  <div>
+                    <h3 className="text-md font-medium text-[var(--primary)] lg:text-xl">
+                      {pd.name}
+                    </h3>
+                    <p className="text-[10px] text-[var(--primary)]/80 lg:text-sm">
+                      {pd.description}
+                    </p>
                   </div>
-                </div>
-
-                <div>
-                  <h3 className="text-sm font-medium text-[var(--accent)] lg:text-xl">
-                    Stack
-                  </h3>
-                  <div className="flex flex-wrap">
-                    {pd.stackIcons.map((tech, index) => (
-                      <a
-                        key={index}
-                        href={tech.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title={tech.name}
-                        className="cursor-pointer grayscale hover:scale-110 hover:grayscale-0"
-                      >
-                        <div className="flex h-7 w-8 items-center justify-center lg:h-10 lg:w-15">
-                          {React.cloneElement(tech.icon, {
-                            className: `${tech.icon.props.className} w-5 h-5 lg:w-8 lg:h-8`,
-                          })}
-                        </div>
-                      </a>
-                    ))}
-                  </div>
-                </div>
-
-                <div className="flex w-fit gap-2 place-self-end">
-                  <div className="flex flex-1 items-end justify-end grayscale">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={pd.sourceCode}
-                      className="group relative inline-flex h-5 items-center gap-2 overflow-hidden rounded-sm bg-[rgba(255,255,255,0.05)] px-3 py-2 text-[10px] text-[var(--primary)] transition duration-300 hover:bg-[rgba(255,255,255,0.08)] hover:shadow-[0_0_12px_rgba(2,218,222,0.2)] lg:h-10 lg:rounded-lg lg:px-6 lg:text-lg"
-                    >
-                      <span className="flex items-center gap-2 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
-                        <GrGithub className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
-                        Code
+                  <div className="text-sm font-medium text-[var(--muted)] lg:text-xl">
+                    <div className="flex flex-col">
+                      <span>Language</span>
+                      <span className="text-[var(--primary)]/80">
+                        {pd.language}
                       </span>
-                      <span className="absolute inset-0 flex translate-x-12 items-center justify-center gap-2 opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
-                        <GrGithub className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
-                        Code
-                        <FiExternalLink className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
-                      </span>
-                    </a>
+                    </div>
                   </div>
-                  {pd.liveDemoUrl && (
+                </div>
+                <div className="flex flex-row justify-between gap-4">
+                  <div>
+                    <h3 className="text-sm font-medium text-[var(--accent)] lg:text-xl">
+                      Stack
+                    </h3>
+                    <div className="flex flex-wrap">
+                      {pd.stackIcons.map((tech, index) => (
+                        <a
+                          key={index}
+                          href={tech.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          title={tech.name}
+                          className="cursor-pointer grayscale hover:scale-110 hover:grayscale-0"
+                        >
+                          <div className="flex h-7 w-8 items-center justify-center lg:h-10 lg:w-15">
+                            {React.cloneElement(tech.icon, {
+                              className: `${tech.icon.props.className} w-5 h-5 lg:w-8 lg:h-8`,
+                            })}
+                          </div>
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex w-fit gap-2 place-self-end">
                     <div className="flex flex-1 items-end justify-end grayscale">
                       <a
                         target="_blank"
                         rel="noopener noreferrer"
-                        href={pd.liveDemoUrl}
+                        href={pd.sourceCode}
                         className="group relative inline-flex h-5 items-center gap-2 overflow-hidden rounded-sm bg-[rgba(255,255,255,0.05)] px-3 py-2 text-[10px] text-[var(--primary)] transition duration-300 hover:bg-[rgba(255,255,255,0.08)] hover:shadow-[0_0_12px_rgba(2,218,222,0.2)] lg:h-10 lg:rounded-lg lg:px-6 lg:text-lg"
                       >
-                        <span className="flex items-center gap-2 whitespace-nowrap transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
-                          <FiGlobe className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
-                          Live Demo
+                        <span className="flex items-center gap-2 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+                          <GrGithub className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
+                          Code
                         </span>
-                        <span className="absolute inset-0 flex translate-x-12 items-center justify-center gap-2 whitespace-nowrap opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
-                          <FiGlobe className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
-                          Live Demo
+                        <span className="absolute inset-0 flex translate-x-12 items-center justify-center gap-2 opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+                          <GrGithub className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
+                          Code
                           <FiExternalLink className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
                         </span>
                       </a>
                     </div>
-                  )}
+                    {pd.liveDemoUrl && (
+                      <div className="flex flex-1 items-end justify-end grayscale">
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href={pd.liveDemoUrl}
+                          className="group relative inline-flex h-5 items-center gap-2 overflow-hidden rounded-sm bg-[rgba(255,255,255,0.05)] px-3 py-2 text-[10px] text-[var(--primary)] transition duration-300 hover:bg-[rgba(255,255,255,0.08)] hover:shadow-[0_0_12px_rgba(2,218,222,0.2)] lg:h-10 lg:rounded-lg lg:px-6 lg:text-lg"
+                        >
+                          <span className="flex items-center gap-2 whitespace-nowrap transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+                            <FiGlobe className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
+                            Live Demo
+                          </span>
+                          <span className="absolute inset-0 flex translate-x-12 items-center justify-center gap-2 whitespace-nowrap opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+                            <FiGlobe className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
+                            Live Demo
+                            <FiExternalLink className="h-2 w-2 text-[var(--primary)] lg:h-5 lg:w-5" />
+                          </span>
+                        </a>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
